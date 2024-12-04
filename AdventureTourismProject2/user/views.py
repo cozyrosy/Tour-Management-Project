b215ca79875056451ea7f9599cbe75391d3d4a33
cozyrosy <gracejacob.rose@gmail.com>
2024-12-04 21:43:36 +0530

    Updated the manager login and homepage, added field in client details.

--- AdventureTourismProject2/user/views.py
+++ AdventureTourismProject2/user/views.py
@@ -14,30 +14,37 @@ def home(request):
     return render(request,'user/home.html', {'tours': tours})
 
 def Register(request):
-    if request.method=='POST':
-        f_name=request.POST['c_fname']
-        l_name = request.POST['c_lname']
-        mail = request.POST['c_email']
-        contact = request.POST['c_phone']
-        password = request.POST['c_password']
-        cpassword = request.POST['c_repassword']
-        address = request.POST['c_address']
-        photo = request.FILES['c_image']
+    try:
+        if request.method=='POST':
+            f_name=request.POST['c_fname']
+            l_name = request.POST['c_lname']
+            mail = request.POST['c_email']
+            contact = request.POST['c_phone']
+            password = request.POST['c_password']
+            cpassword = request.POST['c_repassword']
+            address = request.POST['c_address']
+            photo = request.FILES['c_image']
+            is_manager = request.POST['is_manager']
 
-        if password==cpassword:
-            if User.objects.filter(username=mail).exists():
-                messages.info(request,'email already exists')
+            if password==cpassword:
+                if User.objects.filter(username=mail).exists():
+                    messages.info(request,'email already exists')
 
+                else:
+                    user=User.objects.create_user(username=mail,password=password)
+                    customer=ClientDetails(id=user,first_name=f_name,last_name=l_name,phone=contact,
+                                            email=mail,address=address, photo=photo)
+                    if is_manager=='true':
+                        customer.is_manager=True
+
+                    user.save()
+                    customer.save()
+                    return redirect('user:login')
             else:
-                user=User.objects.create_user(username=mail,password=password)
-                customer=ClientDetails(id=user,first_name=f_name,last_name=l_name,phone=contact,
-                                        address=address, photo=photo)
-                user.save()
-                customer.save()
-                return redirect('user:login')
-        else:
-            messages.info(request,'password mismatch')
-    return render(request,'user/Register.html')
+                messages.info(request,'password mismatch')
+        return render(request,'user/Register.html')
+    except Exception as e:
+        return render(request,'user/Register.html',{'error':e})
 
 def Login(request):
 
@@ -46,9 +53,12 @@ def Login(request):
         password=request.POST['c_password']
         user=auth.authenticate(username=mail,password=password)
 
-        if user is not None and ClientDetails.objects.filter(id=user.id).exists():
+        if user is not None and ClientDetails.objects.filter(id=user.id, is_manager=False).exists():
             auth.login(request,user)
-            return redirect('user/profile')
+            return redirect('/home')
+        elif user is not None and ClientDetails.objects.filter(id=user.id, is_manager=True).exists():
+            auth.login(request,user)
+            return redirect('Manager:home')
         else:
             messages.info(request,'invalid credential')
     return render(request,'user/login.html')
